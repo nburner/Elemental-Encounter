9f8ae71638e2cb398765164467df1e681b0339bb
Nick Terrill <nburner@harding.edu>
2018-01-30 17:48:48 -0600

    Added a static Camera
    When you start the game you can no longer move around the world
    - also included in this is a new gameMode
    - adding a static camera
    - also added a MouseControler Blueprint
    
    Nick Terrill

---
+++ ElementalEncounter/Source/ElementalEncounter/BoardSpace.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BoardSpace.h"
+
+
+// Sets default values
+ABoardSpace::ABoardSpace()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABoardSpace::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABoardSpace::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
