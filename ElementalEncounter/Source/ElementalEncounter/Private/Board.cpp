dcbce39161e91710757e45744ddd34b5530a5adc
Nick <nterrill@harding.edu>
2018-02-01 19:45:47 -0600

    Working on Intergrating Game core with GUI
    I also did some file organizing
    - moved files to more appropriate folders

--- ElementalEncounter/Source/ElementalEncounter/Private/Board.cpp
+++ ElementalEncounter/Source/ElementalEncounter/Private/Board.cpp
@@ -12,16 +12,60 @@ ABoard::ABoard()
 }
 
 // Called when the game starts or when spawned
-void ABoard::BeginPlay()
+//void ABoard::BeginPlay()
+//{
+//	Super::BeginPlay();
+//	
+//}
+//
+//// Called every frame
+//void ABoard::Tick(float DeltaTime)
+//{
+//	Super::Tick(DeltaTime);
+//
+//}
+
+
+ABoard::ABoard()
 {
-	Super::BeginPlay();
-	
+	unsigned char bar = 219;
+
+	for (int i = 0; i < 64; i++) {
+		if ((i / 8 + i) % 2 == 0) space[i] = bar;
+		else space[i] = ' ';
+	}
+
+	for (int i = A1; i <= H2; i++) space[i] = 'W';
+
+	for (int i = A7; i <= H8; i++) space[i] = 'B';
 }
 
-// Called every frame
-void ABoard::Tick(float DeltaTime)
+ABoard::~ABoard()
 {
-	Super::Tick(DeltaTime);
+	instance = NULL;
+}
+
+ABoard* ABoard::getInstance()
+{
+	if (instance == NULL) {
+		instance = new ABoard();
+	}
+
+	return instance;
 
 }
+
+bool ABoard::hasOwnPiece(string space)
+{
+	return true;
+}
+
+void ABoard::updateBoard(move move, Turn t)
+{
+	space[move.first] = ((move.first / 8 + move.first) % 2 == 0 ? 219 : ' ');
+	space[move.second] = (t == 0 ? 'W' : 'B');
+	justTaken = t;
+}
+
+ABoard* ABoard::instance = NULL;
 
