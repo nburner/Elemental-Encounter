5c68fd746a8b814b727cb2ac33b6970092e12f61
Chief of Networking <destrada@harding.edu>
2018-02-10 17:47:55 -0600

    Gameboard and Pieces
    This project has a turn-based breakthrough game with a non-textured
    board and the .fbx pieces Elliot made (pieces also do not have
    textures).

---
+++ Potential Replacement Project/Assets/Scripts/BoardHighlights.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardHighlights : MonoBehaviour
+{
+    public static BoardHighlights Instance { get; set; }
+
+    public GameObject highlightPrefab;
+    private List<GameObject> highlights;
+
+    public void Start()
+    {
+        Instance = this;
+    }
+
+    private GameObject GetHighlightObject()
+    {
+        GameObject go = highlights.Find(g => !g.activeSelf);
+        if (go == null)
+        {
+            go = Instantiate(highlightPrefab);
+            highlights.Add(go);
+        }
+        return go;
+    }
+    public void HighlightAllowedMoves(bool[,] moves)
+    {
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; i++)
+            {
+                if (moves[i, j])
+                {
+                    GameObject go = GetHighlightObject();
+                    go.SetActive(true);
+                    go.transform.position = new Vector3(i, 0, j);
+                }
+            }
+        }
+    }
+
+    public void HideHighlights()
+    {
+        foreach (GameObject go in highlights)
+        {
+            go.SetActive(false);
+        }
+    }
+}
+
---
+++ Potential Replacement Project/Assets/Scripts/BoardManager.cs
@@ -0,0 +1,152 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardManager : MonoBehaviour
+{
+    public Breakman[,] Breakmans { set; get; }
+    private Breakman selectedBreakman;
+
+    private const float TILE_SIZE = 1.0f;
+    private const float TILE_OFFSET = 0.5f;
+    private int selectionX = -1;
+    private int selectionY = -1;
+    
+    //List of Gameobject for spawning pieces on the board
+    public List<GameObject> breakmanPrefabs;
+    private List<GameObject> activeBreakman;
+
+    public bool isFireTurn = true;
+
+    private void Start()
+    {
+        SpawnAllBreakPieces();
+    }
+    private void Update()
+    {
+        UpdateSelection();
+        DrawBoard();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(selectionX>=0 && selectionY >= 0)
+            {
+                if (selectedBreakman == null)
+                {
+                    // select the Breakman
+                    SelectBreakMan(selectionX, selectionY);
+                }
+                else
+                {
+                    // Move the Breakman
+                    MoveBreakman(selectionX, selectionY);
+                }
+            }
+        }
+    }
+
+    private void SelectBreakMan(int x, int y)
+    {
+        if (Breakmans[x, y] == null)
+            return;
+
+        if (Breakmans[x, y].isFire != isFireTurn)
+            return;
+        selectedBreakman = Breakmans[x, y];
+    }
+
+    private void MoveBreakman(int x, int y)
+    {
+        if (selectedBreakman.PossibleMove(x, y))
+        {
+            Breakmans[selectedBreakman.CurrentX, selectedBreakman.CurrentY] = null;
+            selectedBreakman.transform.position = GetTileCenter(x, y);
+            Breakmans[x, y] = selectedBreakman;
+            isFireTurn = !isFireTurn;
+        }
+
+        selectedBreakman = null;
+        
+    }
+    private void UpdateSelection()
+    {
+        if (!Camera.main)
+            return;
+
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 25.0f, LayerMask.GetMask("BreakPlane")))
+        {
+            selectionX = (int)hit.point.x;
+            selectionY = (int)hit.point.z;
+        }
+        else
+        {
+            selectionX = -1;
+            selectionY = -1;
+        }
+    }
+
+    private void DrawBoard()
+    {
+        Vector3 widthLine = Vector3.right * 8;
+        Vector3 heightLine = Vector3.forward * 8;
+
+        for (int i = 0; i <= 8; i++)
+        {
+            Vector3 start = Vector3.forward * i;
+            Debug.DrawLine(start, start + widthLine);
+            for (int j = 0; j <= 8; j++)
+            {
+                start = Vector3.right * j;
+                Debug.DrawLine(start, start + heightLine);
+            }
+        }
+
+        //Draw the Selection
+
+        if (selectionX >= 0 && selectionY >= 0)
+        {
+            Debug.DrawLine(
+                Vector3.forward * selectionY + Vector3.right * selectionX,
+                Vector3.forward * (selectionY + 1) + Vector3.right * (selectionX + 1));
+            Debug.DrawLine(
+                Vector3.forward * (selectionY+1) + Vector3.right * selectionX,
+                Vector3.forward * selectionY + Vector3.right * (selectionX + 1));
+        }
+    }
+
+    private void SpawnBreakman(int index, int x , int y)
+    {
+        GameObject go = Instantiate(breakmanPrefabs[index], GetTileCenter(x,y), Quaternion.identity) as GameObject;
+        go.transform.SetParent(transform);
+        Breakmans[x, y] = go.GetComponent<Breakman>();
+        Breakmans[x, y].SetPosition(x, y);
+        activeBreakman.Add(go);
+    }
+
+    private Vector3 GetTileCenter(int x, int y)
+    {
+        Vector3 origin = Vector3.zero;
+        origin.x += (TILE_SIZE * x) + TILE_OFFSET;
+        origin.z += (TILE_SIZE * y) + TILE_OFFSET;
+        return origin;
+    }
+
+    private void SpawnAllBreakPieces()
+    {
+        activeBreakman = new List<GameObject>();
+        Breakmans = new Breakman[8, 8];
+        //spawn Fire team
+        for(int i=0; i < 8; i++)
+        {
+            SpawnBreakman(0,i,0);
+            SpawnBreakman(0,i, 1);
+        }
+        //spawn Ice team
+        for (int i = 0; i < 8; i++)
+        {
+            SpawnBreakman(1,i, 6);
+            SpawnBreakman(1,i, 7);
+        }
+    }
+}
