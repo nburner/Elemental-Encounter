c8ec66d985381dc8f7ac3316c133451e41862208
bagley2014 <dbagley@harding.edu>
2018-02-10 06:24:13 -0600

    Added a DLL Project
     - Added a DLL project
     - Tested it to make sure it could be accessed from the C# project
     - Changed A LOT of the configuration, to insure the DLL would be built in general and built to the correct place
     - I plan to use this for the AI

--- CSharpSolution/GameCore/Program.cs
+++ CSharpSolution/GameCore/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using GameCore.Core;
@@ -9,12 +10,17 @@ namespace GameCore
 {
 	class Program
 	{
+		[DllImport("AILibrary.dll")]
+		public static extern void DisplayHelloFromDLL();
 		static void Main(string[] args)
 		{
 			Daryl daryl = new Daryl();
 			Human human = new Human();
 
 			Game game = new Game();
+
+			DisplayHelloFromDLL();
+
 			game.newGame(daryl, human, true);
 
 			Console.WriteLine("Winner: {0}", game.Winner.ToString());
