3074ae3d5309dd14560c34f2238fc2c4c2bb0d10
King of Networking <destrada@harding.edu>
2018-02-14 14:13:58 -0600

    Network Components Added
    -Added Network Menu
    -Menus for connecting and hosting a game
    -Refactored the project hierarchy a little
    
    -Derick and Nathan

---
+++ Potential Replacement Project/Assets/Scripts/Networking/NetworkGameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkGameManager : MonoBehaviour
+{ 
+    public static NetworkGameManager Instance { get; set; }
+    public GameObject menu;
+    public GameObject hostMenu;
+    public GameObject clientMenu;
+
+    void Start ()
+    {
+        Instance = this;
+        hostMenu.SetActive(false);
+        clientMenu.SetActive(false);
+        DontDestroyOnLoad(gameObject);
+	}
+
+    public void OnConnectButtonClick()
+    {
+        menu.SetActive(false);
+        clientMenu.SetActive(true);
+    }
+
+    public void OnHostButtonClick()
+    {
+        menu.SetActive(false);
+        hostMenu.SetActive(true);
+    }
+
+    public void OnJoinButtonClick()
+    {
+        Debug.Log("Joining Server...");
+    }
+
+    public void OnBackButtonClick()
+    {
+        menu.SetActive(true);
+        hostMenu.SetActive(false);
+        clientMenu.SetActive(false);
+    }
+}
