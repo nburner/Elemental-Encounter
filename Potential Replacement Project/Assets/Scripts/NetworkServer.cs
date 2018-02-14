3cf011fccfb3736c1a568880d327f2c4170dd918
King of Networking <destrada@harding.edu>
2018-02-13 21:10:05 -0600

    Added code for Networking

--- Potential Replacement Project/Assets/Scripts/NetworkServer.cs
+++ Potential Replacement Project/Assets/Scripts/NetworkServer.cs
@@ -2,22 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net.Sockets;
+using System;
+using System.Net;
 
 public class NetworkServer : MonoBehaviour
 {
-    //public int port = 6321;
+    public int port = 6321;
 
-    //private List<ServerClient> client;
-    //private List<ServerClient> client2;
+    private List<ServerClient> clients;
+    private List<ServerClient> disconnectList;
+
+    private TcpListener server;
+    private bool serverStarted;
+
+    public void Init()
+    {
+        DontDestroyOnLoad(gameObject);
+        clients = new List<ServerClient>();
+        disconnectList = new List<ServerClient>();
+        try
+        {
+            server = new TcpListener(IPAddress.Any, port);
+            server.Start();
+
+            StartListening();
+            serverStarted = true;
+        }
+        catch(Exception e)
+        {
+            Debug.Log("Socket error: " + e.Message);
+        }
+    }
+
+    private void StartListening()
+    {
+        server.BeginAcceptSocket(AcceptTcpClient, server);
+    }
+    private void AcceptTcpClient(IAsyncResult ar)
+    {
+        TcpListener listener = (TcpListener)ar.AsyncState;
+
+        ServerClient sc = new ServerClient(listener.EndAcceptTcpClient(ar));
+        clients.Add(sc);
+
+        StartListening();
+
+        Debug.Log("Somebody has connected!");
+    }
 }
 
-//public class SeverClient
-//{
-//    //public string clientName;
-//    //public TcpClient tcp;
+public class ServerClient
+{
+    public string clientName;
+    public TcpClient tcp;
 
-//    //public ServerClient(TcpClient tcp)
-//    //{
-//    //    this.tcp = tcp;
-//    //}
-//}
+    public ServerClient(TcpClient tcp)
+    {
+        this.tcp = tcp;
+    }
+}
