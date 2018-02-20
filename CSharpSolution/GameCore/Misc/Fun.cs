4a06ddf4786976c4e662dd608f95514a4b6b86cc
Daryl <dbagley@harding.edu>
2018-02-20 14:15:45 -0600

    Cleaned up some stuff
     - Threw my existing main into functions
     - Made a new, empty class lol

---
+++ CSharpSolution/GameCore/Misc/Fun.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace GameCore.Misc
+{
+	class Fun
+	{
+		
+	}
+}
--- CSharpSolution/GameCore/Program.cs
+++ CSharpSolution/GameCore/Program.cs
@@ -17,22 +17,23 @@ namespace GameCore
 			AI seeker = new AI(AIType.SEEKER);
 			AI pet = new AI(AIType.DARYLS_PET);
 			
-			//for (int i = 0; i < 250; i++) {
-			//	AI rand = new AI(AIType.B_RANDOM);
-			//	AI rand2 = new AI(AIType.RANDOM_PET);
+			
 
-			//	playAIMatch(seeker, rand);
-			//	playAIMatch(seeker, rand2);
-			//	playAIMatch(rand, seeker);
-			//	playAIMatch(rand2, seeker);
+            
+		}
 
-			//	playAIMatch(pet, rand);
-			//	playAIMatch(pet, rand2);
-			//	playAIMatch(rand, pet);
-			//	playAIMatch(rand2, pet);
-			//}
+        private static void functionForNathan()
+        {
+            Console.WriteLine("Hello Nathan. This process should be as painful, er, painless as possible.");
+            Console.WriteLine("First, determine what side the AI will play as.");
+            Console.WriteLine("W or B?");
+        }
+        private static void playThatOneTestCase()
+        {
+            AI seeker = new AI(AIType.SEEKER);
+            AI pet = new AI(AIType.DARYLS_PET);
 
-            game.newGame(seeker, 
+            game.newGame(seeker,
                 new[]{Square.A2,
                     Square.B1, Square.B2, Square.B3,
                     Square.C1, Square.C2, Square.C4,
@@ -40,20 +41,40 @@ namespace GameCore
                     Square.E2,
                     Square.F1, Square.F2,
                     Square.G1, Square.G2, Square.G5,
-                    Square.H2}, 
-                pet, 
+                    Square.H2},
+                pet,
                 new[]{Square.A8,
-                    Square.B8, 
-                    Square.C8, 
+                    Square.B8,
+                    Square.C8,
                     Square.D8, Square.D4,
                     Square.E8, Square.E6, Square.E4,
                     Square.F8, Square.F7, Square.F4,
                     Square.G8, Square.G7,
-                    Square.H8, Square.H7}, 
-                Turn.WHITE, true );
-		}
+                    Square.H8, Square.H7},
+                Turn.WHITE, true);
+        }
+        private static void aiCompare(AIType ai1, AIType ai2, int count = 250)
+        {
+            AI _1 = new AI(ai1);
+            AI _2 = new AI(ai2);
 
-		private static void playAIMatch(AI ai1, AI ai2, bool verbose = true) {
+            for (int i = 0; i < 250; i++)
+            {
+                AI rand = new AI(AIType.B_RANDOM);
+                AI rand2 = new AI(AIType.RANDOM_PET);
+
+                playAIMatch(_1, rand);
+                playAIMatch(_1, rand2);
+                playAIMatch(rand, _1);
+                playAIMatch(rand2, _1);
+
+                playAIMatch(_2, rand);
+                playAIMatch(_2, rand2);
+                playAIMatch(rand, _2);
+                playAIMatch(rand2, _2);
+            }
+        }
+        private static void playAIMatch(AI ai1, AI ai2, bool verbose = true) {
 			using (StreamWriter file = File.AppendText("log.txt")) {
 				Console.SetOut(file);
 				game.newGame(ai1, ai2, verbose);
