da129c19369fa46d49fc106db71f4c2ccf3e09f8
nburner <nburner@harding.edu>
2018-02-27 22:26:45 -0600

    Refactored BoardManager Class

--- ElementalEncounter/Assets/Scripts/Piece.cs
+++ ElementalEncounter/Assets/Scripts/Piece.cs
@@ -7,6 +7,7 @@ public class Piece : MonoBehaviour
     public int CurrentX { set; get; }
     public int CurrentY { set; get; }
     public bool isIce;
+    public static BoardManager bm;
 
     public void SetPosition(int x,int y)
     {
@@ -86,5 +87,70 @@ public class Piece : MonoBehaviour
         }
 
         return r;
+    }
+
+    public static void playAnimation(Piece selectedPiece, char moveDirection, int x, int y, bool capture)
+    {
+        bm = BoardManager.Instance;
+
+        if (capture == false)
+        {
+            //If Movement is Left, plays left animation
+            if (moveDirection == 'l')
+            {
+                selectedPiece.GetComponent<Animation>().Play("Left");
+            }
+
+            //If Movement is Right, plays right animation
+            else if (moveDirection == 'r')
+            {
+                selectedPiece.GetComponent<Animation>().Play("Right");
+            }
+
+            //If movement is Forward, plays forward animation
+            else
+            {
+                selectedPiece.GetComponent<Animation>().Play("Forward");
+            }
+        }
+        else
+        {
+            if (bm.isIceTurn == false)
+            {
+                //If capture is Left, plays left capture animation
+                if (moveDirection == 'l')
+                {
+                    selectedPiece.GetComponent<Animation>().Play("LeftBreak");
+                    bm.SpawnBreakman(5, x, y);
+                    Destroy(bm.Breakmans[x, y], 3f);
+                }
+
+                //If capture is Right, plays right capture animation
+                else
+                {
+                    selectedPiece.GetComponent<Animation>().Play("RightBreak");
+                    bm.SpawnBreakman(4, x, y);
+                    Destroy(bm.Breakmans[x, y], 3f);
+                }
+            }
+            else
+            {
+                //If capture is Left, plays left capture animation
+                if (moveDirection == 'l')
+                {
+                    selectedPiece.GetComponent<Animation>().Play("LeftBreak");
+                    bm.SpawnBreakman(2, x, y);
+                    Destroy(bm.Breakmans[x, y], 3f);
+                }
+
+                //If capture is Right, plays right capture animation
+                else
+                {
+                    selectedPiece.GetComponent<Animation>().Play("RightBreak");
+                    bm.SpawnBreakman(3, x, y);
+                    Destroy(bm.Breakmans[x, y], 3f);
+                }
+            }
+        }
     }
 }
--- ElementalEncounter/Assets/Scripts/SinglePlayer/BoardManager.cs
+++ ElementalEncounter/Assets/Scripts/SinglePlayer/BoardManager.cs
@@ -1,41 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Runtime.InteropServices;
+﻿using System.Collections.Generic;
 using UnityEngine;
-using bitboard = System.UInt64;
-using System.Threading;
 
 public class BoardManager : MonoBehaviour
 {
     public enum Turn { ICE, FIRE };
+
+    //List of Gameobject for spawning pieces on the board
+    public List<GameObject> breakmanPrefabs;
+    public List<GameObject> boardPrefabs;
+    public List<GameObject> activeBreakman;
+
+    public bool isIceTurn = true;
+
     public static BoardManager Instance { set; get; }
     public char[,] AllowedMoves { set; get; }
-    public char[,] aiAllowedMoves { set; get; }
-
     public Piece[,] Breakmans { set; get; }
     public bool[,] Spacesboard { set; get; }
+
+    private AIMovement ai;
     private Piece selectedBreakman;
     private bool isClicked = false;
-
     private const float TILE_SIZE = 1.0f;
     private const float TILE_OFFSET = 0.5f;
     private int selectionX = -1;
     private int selectionY = -1;
 
-    //List of Gameobject for spawning pieces on the board
-    public List<GameObject> breakmanPrefabs;
-    public List<GameObject> boardPrefabs;
-    private List<GameObject> activeBreakman;
-
-    public bool isIceTurn = true;
-
-    [DllImport("Assets/Plugins/AILibrary.dll")]
-    public static extern void Seeker(bitboard white, bitboard black, Turn t, ref int from, ref int to);
 
     private void Start()
     {
         Instance = this;
+        ai = new AIMovement();
         SpawnAllBoardSpaces();
         SpawnAllBreakPieces();
     }
@@ -106,11 +100,11 @@ public class BoardManager : MonoBehaviour
                 // Capture a piece
                 activeBreakman.Remove(b.gameObject);
                 Destroy(b.gameObject);
-                playAnimation(selectedBreakman, temp, x, y, true);
+                Piece.playAnimation(selectedBreakman, temp, x, y, true);
             }
             else
             {
-                playAnimation(selectedBreakman, temp, x, y, false);
+                Piece.playAnimation(selectedBreakman, temp, x, y, false);
             }
 
             if (isIceTurn)
@@ -138,7 +132,7 @@ public class BoardManager : MonoBehaviour
 
 
             isIceTurn = !isIceTurn;
-            MakeAIMove(isIceTurn ? Turn.ICE : Turn.FIRE);
+            ai.MakeAIMove();
 
         }
 
@@ -150,62 +144,6 @@ public class BoardManager : MonoBehaviour
 
         selectedBreakman = null;
 
-    }
-
-    private void MakeAIMove(Turn t)
-    {
-        //Convert Gameboard
-        bitboard white = 0;
-        bitboard black = 0;
-        for (int x = 0; x < 8; x++)
-            for (int y = 0; y < 8; y++)
-                if (Breakmans[x, y] != null)
-                {
-                    if (Breakmans[x, y].isIce) { white = AI.Board.set(white, y * 8 + x); }
-                    if (!Breakmans[x, y].isIce) { black = AI.Board.set(black, y * 8 + x); }
-                }
-
-        int from = 48; int to = 41;
-        Seeker(white, black, t, ref from, ref to);
-        //Debug.Log(from.ToString() + " -> " + to.ToString());
-
-        int toX = to % 8;
-        int toY = to / 8;
-        int fromX = from % 8;
-        int fromY = from / 8;
-
-        aiAllowedMoves = Breakmans[fromX, fromY].PossibleMove();
-        char aiTemp = aiAllowedMoves[toX, toY];
-
-        if (Breakmans[toX, toY] != null)
-        {
-            // Capture a piece
-            activeBreakman.Remove(Breakmans[toX, toY].gameObject);
-            Destroy(Breakmans[toX, toY].gameObject, .5f);
-
-            playAnimation(Breakmans[fromX, fromY], aiTemp, toX, toY, true);
-        }
-        else
-        {
-            playAnimation(Breakmans[fromX, fromY], aiTemp, toX, toY, false);
-        }
-
-        //Piece selectedBreakman = Breakmans[fromX, fromY];
-        if ((isIceTurn && Breakmans[fromX, fromY].CurrentY + 1 == 7) || (!isIceTurn && Breakmans[fromX, fromY].CurrentY - 1 == 0))
-        {
-            EndGame();
-            return;
-        }
-
-        
-
-        Breakmans[fromX, fromY].transform.position = GetTileCenter(toX, toY);
-        Breakmans[fromX, fromY].SetPosition(toX, toY);
-
-        Breakmans[toX, toY] = Breakmans[fromX, fromY];
-        Breakmans[fromX, fromY] = null;
-
-        isIceTurn = !isIceTurn;
     }
 
     private void UpdateSelection()
@@ -255,7 +193,7 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    private void SpawnBreakman(int index, int x, int y)
+    public void SpawnBreakman(int index, int x, int y)
     {
         GameObject go = Instantiate(breakmanPrefabs[index], GetTileCenter(x, y), Quaternion.identity) as GameObject;
         go.transform.SetParent(transform);
@@ -270,7 +208,7 @@ public class BoardManager : MonoBehaviour
         another.transform.SetParent(transform);
     }
 
-    private Vector3 GetTileCenter(int x, int y)
+    public Vector3 GetTileCenter(int x, int y)
     {
         Vector3 origin = Vector3.zero;
         origin.x += (TILE_SIZE * x) + TILE_OFFSET;
@@ -319,7 +257,7 @@ public class BoardManager : MonoBehaviour
         }
     }
 
-    private void EndGame()
+    public void EndGame()
     {
         if (isIceTurn)
         {
@@ -340,69 +278,5 @@ public class BoardManager : MonoBehaviour
         isIceTurn = true; //This is provoking an error Needs to have another winning conditions separetely
         BoardHighlights.Instance.HideHighlights();
         SpawnAllBreakPieces();
-    }
-
-    public void playAnimation(Piece selectedPiece, char moveDirection, int x, int y, bool capture)
-    {
-
-        if (capture == false)
-        {
-            //If Movement is Left, plays left animation
-            if (moveDirection == 'l')
-            {
-                selectedPiece.GetComponent<Animation>().Play("Left");
-            }
-
-            //If Movement is Right, plays right animation
-            else if(moveDirection == 'r')
-            {
-                selectedPiece.GetComponent<Animation>().Play("Right");
-            }
-
-            //If movement is Forward, plays forward animation
-            else
-            {
-                selectedPiece.GetComponent<Animation>().Play("Forward");
-            }
-        }
-        else
-        {
-            if(isIceTurn == false)
-            {
-                //If capture is Left, plays left capture animation
-                if (moveDirection == 'l')
-                {
-                    selectedPiece.GetComponent<Animation>().Play("LeftBreak");
-                    SpawnBreakman(5, x, y);
-                    Destroy(Breakmans[x, y], 3f);
-                }
-
-                //If capture is Right, plays right capture animation
-                else
-                {
-                    selectedPiece.GetComponent<Animation>().Play("RightBreak");
-                    SpawnBreakman(4, x, y);
-                    Destroy(Breakmans[x, y], 3f);
-                }
-            }
-            else
-            {
-                //If capture is Left, plays left capture animation
-                if (moveDirection == 'l')
-                {
-                    selectedBreakman.GetComponent<Animation>().Play("LeftBreak");
-                    SpawnBreakman(2, x, y);
-                    Destroy(Breakmans[x, y], 3f);
-                }
-
-                //If capture is Right, plays right capture animation
-                else
-                {
-                    selectedBreakman.GetComponent<Animation>().Play("RightBreak");
-                    SpawnBreakman(3, x, y);
-                    Destroy(Breakmans[x, y], 3f);
-                }
-            }
-        }
     }
 }
