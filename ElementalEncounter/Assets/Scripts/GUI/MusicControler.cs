bde44eea61b371a0fcabf85af164f7c4fb8e57d0
nburner <nburner@harding.edu>
2018-04-11 22:34:51 -0500

    Main Menu Music
    - Also, functionality to change the volume in the options panel

--- ElementalEncounter/Assets/Scripts/GUI/MusicControler.cs
+++ ElementalEncounter/Assets/Scripts/GUI/MusicControler.cs
@@ -1,18 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class MusicControler : MonoBehaviour {
+public class MusicControler : MonoBehaviour
+{
+    [Header("Set in Inspector")]
+    public AudioClip mainMenuMusic;
+    public AudioClip iceMapMusic;
+    public AudioClip fireMapMusic;
+    public AudioClip clashMapMusic;
+    public Slider slider;
+
+    private AudioSource audioSource;
 
     // Use this for initialization
     private void Awake()
     {
-        GameObject[] objs = GameObject.FindGameObjectsWithTag("Music");
-        if(objs.Length > 1)
+        DontDestroyOnLoad(this);
+
+        audioSource = GameObject.Find("MusicController").GetComponent<AudioSource>();
+        if (!audioSource.isPlaying)
         {
-            Destroy(this.gameObject);
+            audioSource.clip = mainMenuMusic;
+            audioSource.Play();
+        }
+    }
+
+    public void OnVolumeSliderChanged()
+    {
+        audioSource.volume = slider.value;
+    }
+
+    public void PlayMapMusic(GameCore.MapChoice map)
+    {
+        audioSource.Stop();
+
+        if (map == GameCore.MapChoice.CLASH)
+        {
+            audioSource.clip = clashMapMusic;
+        }
+        else if (map == GameCore.MapChoice.FIRE)
+        {
+            audioSource.clip = fireMapMusic;
+        }
+        else
+        {
+            audioSource.clip = iceMapMusic;
         }
 
-        DontDestroyOnLoad(this.gameObject);
+        audioSource.Play();
+    }
+
+    public void PlayDefaultMusic()
+    {
+        audioSource = GameObject.Find("MusicController").GetComponent<AudioSource>();
+        audioSource.Stop();
+        audioSource.clip = mainMenuMusic;
+        audioSource.Play();
     }
 }
