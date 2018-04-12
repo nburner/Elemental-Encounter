a02ad3d9a69110b2000e47fa029c4dd0a2cc7ad5
nburner <nburner@harding.edu>
2018-04-12 00:21:13 -0500

    Button Hover and Text
    - Ok, the text looks awful.  Help me Nick, you are my only hope

---
+++ ElementalEncounter/Assets/Scripts/GUI/ButtonHover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonHover : MonoBehaviour
+{
+    public void OnMouseOver()
+    {
+        Debug.Log("over");
+    }
+
+    public void OnMouseExit()
+    {
+        Debug.Log("exit");
+    }
+}
